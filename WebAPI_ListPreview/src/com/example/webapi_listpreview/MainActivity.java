6f1b87f0aa9e5abc43cd39447b7d53a144ccf79b
harute <kaizar19@gmail.com>
2014-07-27 02:46:54 +0900

    ソート一旦コミット

--- WebAPI_ListPreview/src/com/example/webapi_listpreview/MainActivity.java
+++ WebAPI_ListPreview/src/com/example/webapi_listpreview/MainActivity.java
@@ -8,13 +8,24 @@ import java.util.Map;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.SpannableStringBuilder;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Window;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import com.google.gson.Gson;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements TextWatcher {
+
+    private Map<String, String> data;
+    private EditText mEditText; // 変更を検知するエディットボックス
+    List<Sample> baseSampleList;
+    List<Map<String, String>> baseRetDataList;
+    ListView listView;
 
      @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,10 @@ public class MainActivity extends Activity {
         // ウィンドウタイトルバー非表示
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        // リスナーを仕込むエディットボックス
+        this.mEditText = (EditText)this.findViewById(R.id.editText1);
+        this.mEditText.addTextChangedListener(this);
 
         Http.Request request = new Http.Request();
         request.url = "https://but-pazu-test.ssl-lolipop.jp/test_non2.php";
@@ -33,16 +48,18 @@ public class MainActivity extends Activity {
             // 受信した文字列を取得
             String str = (String) response.value;
 
-            ListView listView = (ListView) findViewById(R.id.listView1);
+            listView = (ListView) findViewById(R.id.listView1);
 
             Gson gson = new Gson();
             Sample[] sampleArray = gson.fromJson(str, Sample[].class);
             // リストに変換
             List<Sample> sampleList = Arrays.asList(sampleArray);
+            //コピー
+            baseSampleList = new ArrayList<Sample>(sampleList);
 
             List<Map<String, String>> retDataList = new ArrayList<Map<String, String>>();
             for (Sample sampleRow : sampleList) {
-                Map<String, String> data = new HashMap<String, String>();
+                data = new HashMap<String, String>();
                 data.put("no", Common.getNo(sampleRow.no, sampleRow.rare));
                 data.put("name", sampleRow.name);
                 data.put("other2", "最大Lv." + sampleRow.lv);
@@ -52,6 +69,7 @@ public class MainActivity extends Activity {
                 retDataList.add(data);
                 //Log.d("Tag", "test3");
             }
+            baseRetDataList = retDataList;
 
             // リストビューに渡すアダプタを生成します。
             SimpleAdapter adapter2 = new SimpleAdapter(this, retDataList,
@@ -62,10 +80,78 @@ public class MainActivity extends Activity {
             listView.setAdapter(adapter2);
         }
     }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count,
+            int after) {
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        // TODO 自動生成されたメソッド・スタブ
+
+        Log.d("tag", "base=" + s.toString());
+        Log.d("tag", "base2=" + s.toString());
+        String str = mEditText.getText().toString();
+        //Log.d("tag", "start:" + start + " before:" + before + " count:" + count);
+
+        List<Map<String, String>> retDataList = new ArrayList<Map<String, String>>();
+        if (str.length() == before) {
+         // ここでテキスト変更後の処理
+            if (str.length() == 0) {
+                retDataList = baseRetDataList;
+            } else {
+                for (Sample sampleRow : baseSampleList) {
+                    //Log.d("tag", "base=" + s.toString());
+                    if (sampleRow.name.indexOf(s.toString()) > -1) {
+                        data = new HashMap<String, String>();
+                        data.put("no", Common.getNo(sampleRow.no, sampleRow.rare));
+                        data.put("name", sampleRow.name);
+                        data.put("other2", "最大Lv." + sampleRow.lv);
+                        data.put("other3", "HP " + sampleRow.hp);
+                        data.put("other4", "攻撃 " + sampleRow.attack);
+                        data.put("other5", "回復 " + sampleRow.recovery);
+                        Log.d("tag", "result=" + sampleRow.name);
+                        retDataList.add(data);
+                    }
+                }
+            }
+
+
+            // リストビューに渡すアダプタを生成します。
+            SimpleAdapter adapter2 = new SimpleAdapter(this, retDataList,
+                    R.layout.raw, new String[] { "no", "name" ,"other2", "other3","other4","other5"},
+                    new int[] {R.id.textView1, R.id.textView2 , R.id.textView4, R.id.textView5, R.id.textView6, R.id.textView7});
+
+            // アダプタを設定します。
+            listView.setAdapter(null);
+            listView.setAdapter(adapter2);
+        } else {
+            if (count == 0) {
+                //Log.d("tag", "test");
+                // リストビューに渡すアダプタを生成します。
+                SimpleAdapter adapter2 = new SimpleAdapter(this, baseRetDataList,
+                        R.layout.raw, new String[] { "no", "name" ,"other2", "other3","other4","other5"},
+                        new int[] {R.id.textView1, R.id.textView2 , R.id.textView4, R.id.textView5, R.id.textView6, R.id.textView7});
+
+                // アダプタを設定します。
+                listView.setAdapter(null);
+                listView.setAdapter(adapter2);
+            }
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
 }
 
 /*
-* Json文字列をこのクラスに変化する
+* Json文字列をこのクラスに変化するa
 */
 class Sample{
     public String no;
