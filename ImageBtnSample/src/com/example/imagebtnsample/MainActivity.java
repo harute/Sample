471dce3a0c1fe285f279c76f4eb3db98088161d2
harute <kaizar19@gmail.com>
2014-07-29 00:17:21 +0900

    ImageBtnファーストコミット

---
+++ ImageBtnSample/src/com/example/imagebtnsample/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.imagebtnsample;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    int width;
+    int height;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //StrictModeを設定 penaltyDeathを取り除く
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
+
+        setContentView(R.layout.activity_main);
+
+        getDisplay();
+        setImageButton(R.id.imgbtn_id01, "001.png");
+        setImageButton(R.id.imgbtn_id02, "002.png");
+        setImageButton(R.id.imgbtn_id03, "003.png");
+        setImageButton(R.id.imgbtn_id04, "004.png");
+        setImageButton(R.id.imgbtn_id05, "001.png");
+        setImageButton(R.id.imgbtn_id06, "002.png");
+
+    }
+
+    private void setImageButton(int imgbtnId, String fileName) {
+        ImageButton img = (ImageButton)findViewById(imgbtnId);
+
+        // 画像のURL
+        String urlString="http://pazu-test.but.jp/image/" + fileName;
+
+        try {
+            //URLクラス
+            URL url = new URL(urlString);
+            //入力ストリームを開く
+            InputStream istream = url.openStream();
+
+            //画像をDrawableで取得
+            Drawable d = Drawable.createFromStream(istream, "webimg");
+
+            //入力ストリームを閉じる
+            istream.close();
+
+            img.setImageDrawable(d);
+            android.view.ViewGroup.LayoutParams params = img.getLayoutParams();
+            params.height = width / 6;
+            params.width = width / 6;
+
+            img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        } catch (Exception e) {
+            System.out.println("nuu: "+e);
+        }
+
+    }
+
+    private void getDisplay() {
+        WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
+        // ディスプレイのインスタンス生成
+        Display disp = wm.getDefaultDisplay();
+        Point size = new Point();
+        disp.getSize(size);
+        width = size.x;
+        height = size.y;
+    }
+}
