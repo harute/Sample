766143647f21965d4d377ec7f0d13bec7b145c88
harute <kaizar19@gmail.com>
2014-08-06 01:18:47 +0900

    覚醒スキルAPI対応

--- ImageBtnSample/src/com/example/imagebtnsample/MainActivity.java
+++ ImageBtnSample/src/com/example/imagebtnsample/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.imagebtnsample;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 import android.app.Activity;
-import android.content.res.Resources;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -18,7 +18,6 @@ import android.view.Display;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -52,43 +51,62 @@ public class MainActivity extends Activity {
         setImageButton(R.id.imgbtn_id05, "002.png");
         setImageButton(R.id.imgbtn_id06, "002.png");
 
-        // ボタン押下時に画面遷移
+        // ボタン押下時に画面遷移 sample
         ImageButton btn = (ImageButton)findViewById(R.id.imgbtn_id01);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Tag", "OKOK");
             }
         });
 
-        // ボタン押下時に画面遷移
+        // ボタン押下時に画面遷移 sample
         ImageButton btn2 = (ImageButton)findViewById(R.id.imgbtn_id02);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Tag", "OKOK222");
             }
         });
 
         // ここからList処理
         listView1 = (ListView)findViewById(R.id.listView1);
 
-        String[][] lst = {
-                {"11111111111111111111111111111111111111111aaaaaaaaaaaaaa", "http://pazu-test.but.jp/image/004.png"},
-                {"OK", "http://pazu-test.but.jp/image/003.png"},
-                {"NG", "http://pazu-test.but.jp/image/002.png"},
-                {"NG1345", "http://pazu-test.but.jp/image/001.png"}
-        };
+        // http処理
+        Http.Request request = new Http.Request();
+        request.url = "https://but-pazu-test.ssl-lolipop.jp/skillSearch.php?leader[0]=1200&leader[1]=1201&member[0]=1234&member[1]=1235&member[2]=1236&member[3]=1236";
+
+        Http.Response response = Http.requestSync(request, StringResponseHandler.getInstance());
+        Sample sample = new Sample();
+        if (response.code == 200) {
+            String str = (String) response.value;
+            Gson gson = new Gson();
+            sample = gson.fromJson(str, Sample.class);
+        }
+
+        @SuppressWarnings("unchecked")
+        ArrayList<String>[] arrtest = new ArrayList[50];
+        arrtest[0] = new ArrayList<String>();
+        arrtest[0].add(sample.ls_desc.split(",")[0] + "\n" + sample.ls_desc.split(",")[1]);
+        arrtest[0].add("http://pazu-test.but.jp/image/004.png");
+
+        // 覚醒
+        int cnt = 1;
+        for (Kakusei kakuraw : sample.kakusei) {
+            arrtest[cnt] = new ArrayList<String>();
+            arrtest[cnt].add(kakuraw.desc);
+            arrtest[cnt].add(kakuraw.url);
+            cnt++;
+        }
+
         // ここで繰り返し処理
         //入力ストリームを開く
         InputStream istream;
         try {
-            for (int i = 0; i < lst.length; i++) {
-                URL url = new URL(lst[i][1]);
+            for (int i = 0; i < cnt; i++) {
+                URL url = new URL(arrtest[i].get(1));
                 istream = url.openStream();
                 Drawable d = Drawable.createFromStream(istream, "webimg");
                 AppInfo bbb = new AppInfo();
-                bbb.setTextData(lst[i][0]);
+                bbb.setTextData(arrtest[i].get(0));
                 bbb.setImagaData(d);
                 items.add(bbb);
             }
@@ -96,7 +114,6 @@ public class MainActivity extends Activity {
             // TODO 自動生成された catch ブロック
             e.printStackTrace();
         }
-
 
         //adapter = new AppInfoArrayAdapter(this, R.layout.raw, R.id.row_textview1, items);
         adapter = new AppInfoArrayAdapter(this, R.layout.raw, items);
@@ -144,4 +161,18 @@ public class MainActivity extends Activity {
         width = size.x;
         height = size.y;
     }
+}
+
+/*
+* Json文字列をこのクラスに変化するa
+*/
+class Sample{
+    public String ls_name;
+    public String ls_desc;
+    public Kakusei[] kakusei;
+}
+
+class Kakusei {
+    public String desc;
+    public String url;
 }
